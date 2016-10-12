fc76ab94df9906aa4af601a03790c2c52e9bfecb
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-10-12 15:36:48 -0500

    Data and images

---
+++ js/products.js
@@ -0,0 +1,65 @@
+var costumes = [
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""	
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""	
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""
+	},
+	{
+		"name": "",
+		"gender": "",
+		"price": "",
+		"description": "",
+		"image": ""	
+	}	
+]
